c930c29fc04f3a309925b4d8b3dbadf9b30e42d4
Sam Hood <samjdhood@gmail.com>
2025-09-30 19:37:57 -0500

    add new field to reflect storing number of arguments per function

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,6 @@ use std::fs;
 mod cpu;
 mod decoder;
 
-/*
-* endian-ness: the 6502 is a little endian system. when we read bytecode, while the first byte will
-* be the opcode, we must keep in mind that reading operands will then have to be flipped (i.e.
-* 0x123456 would need to be read ultimately into our buffer as 56 34 12)
-* */
-
 fn main() {
     // eventually we need to error check this to actually make sure that we are being supplied
     // enough arguments
